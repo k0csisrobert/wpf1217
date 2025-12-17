0515144879eb613f5dd09be8fbae1ca590d5c07e
Robi <kocsisr@kkszki.hu>
2025-12-17 08:12:32 +0100

    1. gomb es az alapok

--- wpf1217/MainWindow.xaml.cs
+++ wpf1217/MainWindow.xaml.cs
@@ -1,20 +1,44 @@
 ﻿using System.Windows;
+using static wpf1217.MainWindow;
 
 namespace wpf1217
 {
     public partial class MainWindow : Window
     {
-
-        public class Termekek()
+        public class ItemModel
         {
-            public string Nev { get; set; }
+            public String Nev { get; set; }
+            public int Mennyiseg { get; set; }
+            public int Ar {  get; set; }
+            public string Kategoria { get; set; }
+            public int Osszesen { get; set; }
 
-            public int Db { get; set; }
-
+            public ItemModel(string nev, int mennyiseg, int ar, string kategoria)
+            {
+                Nev = nev;
+                Mennyiseg = mennyiseg;
+                Ar = ar;
+                Kategoria = kategoria;
+                Osszesen = mennyiseg * ar;
+            }
         }
+
+        List<ItemModel> termekek = new List<ItemModel>();
         public MainWindow()
         {
             InitializeComponent();
+
+            termekek.Add(new ItemModel("Tej", 5, 450, "A"));
+            termekek.Add(new ItemModel("Kenyer", 10, 350, "B"));
+            termekek.Add(new ItemModel("Sajt", 2, 1200, "A"));
+            termekek.Add(new ItemModel("Alma", 20, 200, "C"));
+            termekek.Add(new ItemModel("Narancs", 15, 300, "C"));
+            termekek.Add(new ItemModel("Hús", 3, 2500, "D"));
+            termekek.Add(new ItemModel("Csokoládé", 7, 900, "B"));
+            termekek.Add(new ItemModel("Kenyér", 1, 450, "B"));
+            termekek.Add(new ItemModel("Tej", 12, 400, "A"));
+            termekek.Add(new ItemModel("Sajt", 5, 1500, "D"));
+            termekDG.ItemsSource = termekek;
         }
 
         private void hozzaadas(object sender, RoutedEventArgs e)
