72dd6f45e158108637b0eb644d550071c5b081c4
Robi <kocsisr@kkszki.hu>
2025-12-17 08:03:26 +0100

    Add project files.

---
+++ wpf1217/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace wpf1217
+{
+    public partial class MainWindow : Window
+    {
+
+        public class Termekek()
+        {
+            public string Nev { get; set; }
+
+            public int Db { get; set; }
+
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void hozzaadas(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
